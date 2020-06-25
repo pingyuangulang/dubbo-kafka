610355ca541fac5dc7b6536661a539820c32a313
jib <jib@yunk.ai>
2020-06-25 15:20:39 +0800

    mysql、redis、zookeeper、kafka迁移

--- dubbo-kafka-data/src/main/resources/db.sql
+++ dubbo-kafka-data/src/main/resources/db.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `dubbo_kafka`.`author`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '作者名称',
-  `writeName` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '作者笔名',
+  `write_Name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '作者笔名',
   `age` int(4) UNSIGNED NOT NULL COMMENT '作者年龄',
   `gender` tinyint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '性别。0：未知；1：男；2：女',
   `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
@@ -15,7 +15,7 @@ CREATE TABLE `dubbo_kafka`.`author`  (
 CREATE TABLE `dubbo_kafka`.`book`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '书名',
-  `authorId` bigint(20) UNSIGNED NOT NULL COMMENT '书籍作者',
+  `author_id` bigint(20) UNSIGNED NOT NULL COMMENT '书籍作者',
   `price` decimal(10, 2) NOT NULL DEFAULT 0 COMMENT '书籍单价',
   `deleted` tinyint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态。0：有效；1：删除',
   PRIMARY KEY (`id`)
--- dubbo-kafka-eureka/src/main/java/com/five/monkey/eureka/config/WebSecurityConfig.java
+++ dubbo-kafka-eureka/src/main/java/com/five/monkey/eureka/config/WebSecurityConfig.java
@@ -1,18 +1,18 @@
-package com.five.monkey.eureka.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    /**
-     * Spring Boot 2.0 以上的security默认启用了csrf检验，要在eurekaServer端配置security的csrf检验为false
-     * @param http
-     * @throws Exception
-     */
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        super.configure(http);
-    }
-}
+//package com.five.monkey.eureka.config;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    /**
+//     * Spring Boot 2.0 以上的security默认启用了csrf检验，要在eurekaServer端配置security的csrf检验为false
+//     * @param http
+//     * @throws Exception
+//     */
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        super.configure(http);
+//    }
+//}
