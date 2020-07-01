7a70035b7a2b407dc5963946d4ef9f02be4d39a7
jib <jib@yunk.ai>
2020-07-01 18:40:24 +0800

    添加日志配置文件

--- wrap-spring-boot-starter/src/main/java/com/five/monkey/wrap/service/impl/DefaultWrapServiceImpl.java
+++ wrap-spring-boot-starter/src/main/java/com/five/monkey/wrap/service/impl/DefaultWrapServiceImpl.java
@@ -1,6 +1,7 @@
 package com.five.monkey.wrap.service.impl;
 
 import com.five.monkey.wrap.service.WrapService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  *
@@ -8,6 +9,12 @@ import com.five.monkey.wrap.service.WrapService;
  * @author jim
  * @date 2020/6/24 23:32
  */
+@Slf4j
 public class DefaultWrapServiceImpl implements WrapService {
 
+    @Override
+    public String wrap(String word) {
+        log.warn("wrap operate prefix and suffix is blank");
+        return word;
+    }
 }
--- wrap-spring-boot-starter/src/main/java/com/five/monkey/wrap/service/impl/WrapServiceImpl.java
+++ wrap-spring-boot-starter/src/main/java/com/five/monkey/wrap/service/impl/WrapServiceImpl.java
@@ -2,6 +2,7 @@ package com.five.monkey.wrap.service.impl;
 
 import com.five.monkey.wrap.service.WrapService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  *
@@ -9,6 +10,7 @@ import lombok.AllArgsConstructor;
  * @author jim
  * @date 2020/6/24 21:12
  */
+@Slf4j
 public class WrapServiceImpl implements WrapService {
 
     private String before;
@@ -26,6 +28,8 @@ public class WrapServiceImpl implements WrapService {
 
     @Override
     public String wrap(String word) {
+        String msg = "wrap operate prefix=%s,suffix=%s";
+        log.info(String.format(msg, before, after));
         return before + word + after;
     }
 }
