1326960870cfdf6ba890760bd20fa5d7018918e6
jim <jibin_0310@163.com>
2020-09-16 10:11:20 +0800

    Redis分布式锁释放采用Lua脚本

--- dubbo-kafka-util/src/main/java/com/five/monkey/util/lock/RedisLock.java
+++ dubbo-kafka-util/src/main/java/com/five/monkey/util/lock/RedisLock.java
@@ -3,11 +3,15 @@ package com.five.monkey.util.lock;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 分布式锁
@@ -26,15 +30,14 @@ public class RedisLock {
      *
      * @param key 加锁key
      * @param value 加锁value,释放锁时需要根据value进行判断
-     * @param expire
-     * @param timeUnit
+     * @param expire 过期时间,单位:秒
      * @return true:加锁成功
      */
-    public boolean lock(String key, String value, @NonNull Long expire, @NonNull TimeUnit timeUnit) {
+    public boolean lock(String key, String value, @NonNull Long expire) {
         if (StringUtils.isBlank(key)) {
             throw new IllegalArgumentException("class:RedisLock,method:public boolean lock(String key),error message:key is blank");
         }
-        Boolean result = stringRedisTemplate.boundValueOps(key).setIfAbsent(value, expire, timeUnit);
+        Boolean result = stringRedisTemplate.boundValueOps(key).setIfAbsent(value, expire, TimeUnit.SECONDS);
         return Optional.ofNullable(result).orElse(false);
     }
 
@@ -45,12 +48,10 @@ public class RedisLock {
      * @param value 当释放锁value与加锁value相等时才会释放锁
      * @return true:释放锁成功
      */
-    public boolean unLock(String key, String value) {
-        Boolean result = false;
-        // TODO 此处存在非原子性操作,获取value和删除key不是原子性操作,此处为保证原子性可以使用lua脚本
-        if (StringUtils.equals(value, stringRedisTemplate.boundValueOps(key).get())) {
-            result = stringRedisTemplate.delete(key);
-        }
-        return Optional.ofNullable(result).orElse(false);
+    public void unLock(String key, String value) {
+        String luaScript = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        RedisScript<Integer> script = new DefaultRedisScript<>(luaScript, Integer.class);
+        stringRedisTemplate.execute(script, Stream.of(key).collect(Collectors.toList()), value);
+
     }
 }
